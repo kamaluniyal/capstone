af78410e23203104e8c4239390dfd3fc9ab72f71
prati <pratikshanaithani1@gmail.com>
2021-03-21 19:42:39 +0530

    webcheck.sh

--- WebCheck.sh
+++ WebCheck.sh
@@ -1,6 +1,9 @@
 sleep 10
 webAdress=$(kubectl get svc | grep -i webserver-loadbalancer | awk -F ' ' '{print $4}')
 curl $webAdress > newhtml.txt
+echo $webAdress
+cat newhtml.txt
+
 dos2unix newhtml.txt index.html
 checkForDiff=$(cmp newhtml.txt index.html | wc -l)
 if [[ checkForDiff -gt 0 ]]
